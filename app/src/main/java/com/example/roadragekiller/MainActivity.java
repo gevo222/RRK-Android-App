b58457e150996bffe2e57ebadf44ac72c4315506
gevo222 <51228298+gevo222@users.noreply.github.com>
2019-07-19 10:08:51 -0700

    Created Start button that can be clicked

--- app/src/main/java/com/example/roadragekiller/MainActivity.java
+++ app/src/main/java/com/example/roadragekiller/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.roadragekiller;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
-
-import android.view.MenuItem;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
@@ -40,6 +41,23 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // Start button
+        Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // When user clicks start button do this
+            public void onClick(View view) {
+
+                TextView test = findViewById(R.id.textView); // just testing, should be replaced
+                                                             // later to start the speed tracker
+
+                test.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
     }
 
 }
