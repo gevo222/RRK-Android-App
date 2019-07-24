39d5c69034164c5ab7cdd08846e4b3b28a0d1ab2
aramb208 <53016674+aramb208@users.noreply.github.com>
2019-07-23 19:42:10 -0700

    Settings Button missing Settings Activity

--- app/src/main/java/com/example/roadragekiller/MainActivity.java
+++ app/src/main/java/com/example/roadragekiller/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.roadragekiller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button settingsButton = findViewById(R.id.button_settings);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // When user clicks start button do this
+            public void onClick(View view) {
+                TextView test = findViewById(R.id.button_settings);
+                Intent intent = new Intent(MainActivity.this,SettingsActivity.class);
+                startActivity(intent);
+                //test.setVisibility(View.VISIBLE);           // speed pops up
+                //settingsButton.setVisibility(View.INVISIBLE);  // button goes away
+
+                //onLocationChanged(null);                    // calls the current speed tracker
+            }
+        });
     }
+
 }
