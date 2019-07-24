09bce76d4ac027dad8b6e17735831678434805cb
gevo222 <51228298+gevo222@users.noreply.github.com>
2019-07-23 19:10:23 -0700

    Revert "added stop button. going to make it a thread next."
    This reverts commit 7b72b58e

--- app/src/main/java/com/example/roadragekiller/MainActivity.java
+++ app/src/main/java/com/example/roadragekiller/MainActivity.java
@@ -1,14 +1,111 @@
 package com.example.roadragekiller;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends Activity implements LocationListener {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // checks if we have location permission
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        }
+
+
+        // Start button
+        final Button startButton = findViewById(R.id.startButton);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            // When user clicks start button do this
+            public void onClick(View view) {
+
+                TextView test = findViewById(R.id.userSpeed);
+
+
+                test.setVisibility(View.VISIBLE);           // speed pops up
+                startButton.setVisibility(View.INVISIBLE);  // button goes away
+
+                onLocationChanged(null);                    // calls the current speed tracker
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView txt = (TextView) findViewById(R.id.userSpeed);
+
+        if (location == null) {
+           // txt.setText("-.- m/s");
+        }
+        else{
+            float nCurrentSpeed = location.getSpeed();
+             DecimalFormat df2 = new DecimalFormat("#.##");
+
+            txt.setText(df2.format(nCurrentSpeed/1.944 )+ " m/s");
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
